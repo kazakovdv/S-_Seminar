79f171c0c03437381b3dfc15e3ebf7ec90178f3c
Danil <kazakovdv1@gmail.com>
2022-08-24 22:24:00 +0500

    zadacha 2

---
+++ zadacha_2/Program.cs
@@ -0,0 +1,21 @@
+﻿int Max(int arg1, int arg2)
+{
+    int result = arg1;
+    if(arg2> result) result = arg2;
+    return result;
+}
+
+int a1 = 5;
+int b1 = 7;
+int a2 = 2;
+int b2 = 10;
+int a3 = -9;
+int b3 = -3;
+
+int max1 = Max(a1, b1);
+int max2 = Max(a2, b2);
+int max3 = Max(a3, b3);
+
+Console.WriteLine(max1);
+Console.WriteLine(max2);
+Console.WriteLine(max3);
