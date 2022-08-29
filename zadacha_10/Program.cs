d84adad62a9a4079968f65a333d62c2777ce0f24
Danil <kazakovdv1@gmail.com>
2022-08-29 23:42:24 +0500

    zadacha 10

---
+++ zadacha_10/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите трехзначное число ");
+int n = 0;
+while(n < 1000)
+    {
+        string input = Console.ReadLine();
+        if(!input.Equals("exit"))
+            Console.WriteLine("{0}->{1}",input, input[1]);
+        else
+            break;
+    }
